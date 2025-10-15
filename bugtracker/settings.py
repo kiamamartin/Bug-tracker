90b9f1c069d5536394af7d6cae29099f0a75ff6e
Martin <kiamamartin92@gmail.com>
2025-10-15 20:12:40 +0300

    Implemented static file settings, enhanced login and registration templates, and added CSS for improved styling

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'tickets', 'static'),
+]
 
 # Application definition
 
@@ -116,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
